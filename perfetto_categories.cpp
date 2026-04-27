7139a68edef4ebc8d91d055c68a2a6016bd843a6
EngineerSmith <56016593+EngineerSmith@users.noreply.github.com>
2026-04-27 15:31:49 +0100

    docs: updated README to reflect perfetto issue 5591

--- perfetto_categories.cpp
+++ perfetto_categories.cpp
@@ -1,5 +1,6 @@
 #include "perfetto_categories.h"
 
 // https://github.com/google/perfetto/blob/main/examples/sdk/trace_categories.cc
+// V54-52 have a bug on MSVC, see my issue https://github.com/google/perfetto/issues/5591
 
 PERFETTO_TRACK_EVENT_STATIC_STORAGE();
